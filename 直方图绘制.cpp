c5ea01214492d6c90c9c7f2981a855ad4f1576aa
群星交响曲 <2271462628@qq.com>
2024-01-16 23:32:40 +0800

    添加项目文件。

---
+++ 直方图绘制.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <opencv2/opencv.hpp>
+
+using namespace std;
+using namespace cv;
+void DrawGrayHist(const Mat& srcImg);
+
+int main()
+{
+	Mat srcImg = imread("lena.jpg", 0);
+	if (srcImg.empty())
+	{
+		std::cout << "图片读取失败" << std::endl;
+		return -1;
+	}
+	DrawGrayHist(srcImg);
+	waitKey(0);
+	return 0;
+}
+
+void DrawGrayHist(const Mat& srcImg)
+
+	{
+
+		if (1 != srcImg.channels())
+		{
+			std::cout << "图片通道数不为1" << std::endl;
+		}
+
+		Mat dstHist;
+		int histSize[] = { 256 }; //直方图尺寸
+
+		float midRanges[] = { 0,256 }; //像素值范围
+		const float* ranges[] = { midRanges };
+		calcHist(&srcImg, 1, 0, Mat(), dstHist, 1, histSize, ranges); //计算直方图
+		Mat histImage = Mat::zeros(Size(256, 256), CV_8UC1);
+		double dblHistMaxValue;
+		minMaxLoc(dstHist, 0, &dblHistMaxValue, 0, 0);
+		for (int i = 0; i < 255; i++)
+		{
+			int value = cvRound(dstHist.at<float>(i) * 255 / dblHistMaxValue);//归一化
+			line(histImage, Point(i, histImage.rows - 1), Point(i, histImage.rows - 1 - value), Scalar(255));//绘制直方图
+
+		}
+		imshow("直方图", histImage);
+	
+
+
+
+	}
+
